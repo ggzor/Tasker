cc9818cd66b8f7686a03511588481892b03f32ce
ggzor <30713864+ggzor@users.noreply.github.com>
2017-09-14 17:44:28 -0500

    Adding basic material Look and Feel

---
+++ Tasker/MainView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tasker
+{
+    /// <summary>
+    /// Lógica de interacción para MainView.xaml
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorZone_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (e.ClickCount > 1)
+                    WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+                else
+                    DragMove();
+            }
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+    }
+}
